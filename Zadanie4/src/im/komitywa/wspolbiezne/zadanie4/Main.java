877fce6b2f827aedc0480e1e5442e93a907af97f
wyzellak <b4k4squared@gmail.com>
2013-11-15 14:43:40 +0100

    szkic maina

--- Zadanie4/src/im/komitywa/wspolbiezne/zadanie4/Main.java
+++ Zadanie4/src/im/komitywa/wspolbiezne/zadanie4/Main.java
@@ -1,8 +1,23 @@
 package im.komitywa.wspolbiezne.zadanie4;
 
+import im.komitywa.wspolbiezne.zadanie4.api.Client;
+import im.komitywa.wspolbiezne.zadanie4.api.Server;
+import im.komitywa.wspolbiezne.zadanie4.impl.CEOClient;
+import im.komitywa.wspolbiezne.zadanie4.impl.DocumentStorageServer;
+import im.komitywa.wspolbiezne.zadanie4.impl.OfficeExecutiveClient;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Server skrytki = new DocumentStorageServer(1);
+
+        Client sekretarka = new OfficeExecutiveClient();
+        sekretarka.setServer(skrytki);
+        Client dyrektorPierwszy = new CEOClient();
+        dyrektorPierwszy.setServer(skrytki);
+        Client dyrektorDrugi = new CEOClient();
+        dyrektorDrugi.setServer(skrytki);
+
+
     }
 }
